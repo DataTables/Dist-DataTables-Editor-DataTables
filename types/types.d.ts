68055814f1edf4ca8be4707b58e3d5820e9f7f96
Allan Jardine <allan@datatables.net>
2026-06-23 15:24:11 +0000

    Initial commit

---
+++ types/types.d.ts
@@ -0,0 +1,7 @@
+
+// Dist-DataTables-Editor-DataTables integration with DataTables exports the main Editor class having
+// set default values to complete the ingeration.
+import Editor from "datatables.net-editor";
+
+export default Editor;
+
