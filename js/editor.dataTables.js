5e91e2fe799c6590ecad3cf4f2de7078815cfdae
dtbuild <dtbuild@rigil>
2026-08-18 10:45:55 +0000

    d1f1058106db7369634a9cd975534910a4c5d917 Release 3.0.1
    Sync to source repo @d1f1058106db7369634a9cd975534910a4c5d917

--- js/editor.dataTables.js
+++ js/editor.dataTables.js
@@ -1,4 +1,4 @@
-/*! Editor default styling 3.0.0 for DataTables
+/*! Editor default styling 3.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 
--- js/editor.dataTables.min.mjs
+++ js/editor.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! Editor default styling 3.0.0 for DataTables
+/*! Editor default styling 3.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-dt";import Editor from"datatables.net-editor";export default DataTable.Editor;
